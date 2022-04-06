0af177b0ac8e9bc89dae829862d37d3a98cd3dda
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-04-07 02:26:28 +0900

    feat Main_2557
    feat Main_2557

---
+++ baekjoon/Main_2557.java
@@ -0,0 +1,11 @@
+package com.kh.inputOutput;
+
+// 2022.4.6(수) 23h45
+// 백준 online judge에 제출 시에는 Main.java로 가정해서 받기 때문에 클래스명은 Main으로(컴파일 에러) + 패키지 정보 삭제(런타임 에러 발생) 
+public class Main_2557 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
