81fa0ea16f844890c45f8df32c6101fd90c58eaa
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-04-20 23:49:29 +0900

    feat: create Main_2753.java
    Create Main_2753.java as a solution to a lv1 operators problem

---
+++ baekjoon/lv1/Main_2753.java
@@ -0,0 +1,32 @@
+package com.kh.inputOutput;
+
+import java.util.Scanner;
+
+//2022.4.20(수) 23h30 윤년 -> 23h50 208ms 맞았습니다
+public class Main_2753 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		System.out.print("연도(1~4000 범위의 정수 1개)를 입력해주세요 > ");
+		int year = sc.nextInt();
+		
+		while (true) {
+			if (year < 1 || year > 4000) {
+//				System.out.print("잘못된 범위의 숫자입니다. 연도를 다시 입력해주세요 > ");
+				year = sc.nextInt();
+			} else {
+				break;
+			}
+		}
+		
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)) {
+			System.out.println("1"); // 윤년입니다
+		} else {
+			System.out.println("0"); // 윤년이 아닙니다
+		}
+		
+	}
+	
+}
