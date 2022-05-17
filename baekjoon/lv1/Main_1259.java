3664532fd5d6076c36992e0fd4edc2b4f68f09e1
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-05-17 23:59:05 +0900

    feat: create Main_1259.java
    Create Main_1259.java as a v1 solution to a lv1 String problem

---
+++ baekjoon/lv1/Main_1259.java
@@ -0,0 +1,25 @@
+package com.baekjoon.string;
+
+import java.util.Scanner;
+
+// 2022.5.17(화) 23h50
+public class Main_1259 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			String input = Integer.toString(sc.nextInt());
+			
+			if (!input.equals("0")) {
+				
+			} else {
+				return;
+			}
+			
+		}
+		
+//		sc.close(); // Unreachable code
+	}
+
+}
