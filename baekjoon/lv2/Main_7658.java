4f5c16bf8875a732c7e5319a868ff179295a978a
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-05-19 23:59:30 +0900

    Create Main_7658.java

---
+++ baekjoon/lv2/Main_7658.java
@@ -0,0 +1,21 @@
+package com.baekjoon.control;
+
+import java.util.Scanner;
+
+// 2022.5.19(목) 23h50
+public class Main_7568 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 전체 사람의 수
+		int[] list = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+		
+		sc.close();
+	}
+
+}
