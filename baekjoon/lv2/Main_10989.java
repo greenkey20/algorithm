1699d2fe86113152b76e1d128e895f4813241aaa
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-05-22 23:57:15 +0900

    feat: create Main_10989.java
    Create Main_10989.java as a v1 solution to a lv2 sort problem

---
+++ baekjoon/lv2/Main_10989.java
@@ -0,0 +1,20 @@
+package com.baekjoon.sort;
+
+// 2022.5.22(일) 23h55
+public class Main_10989 {
+	
+	// 주어진 n개의 수를 오름차순으로 정렬하는 프로그램
+	public static void main(String[] args) {
+		/* 정렬
+		 * 1. 내 vs 외부 정렬
+		 * 2. 안정적 vs 불안정적 정렬
+		 * 3. 제자리 정렬
+		 * 4. 비교 기반 vs 데이터 분포 기반 정렬
+		 * 
+		 * 기본 = 버블, 선택, 삽입, 셸 정렬
+		 * 개선된 성능 = 합병, 퀵, 힙
+		 */
+		
+	}
+
+}
