a1d0139557d796628e0871d966aefade958c3229
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-06-04 00:43:32 +0900

    feat: create Main_1436.java
    Create Main_1436.java as a v1 solution to a lv2 brute force problem

--- baekjoon/lv2/Main_1436.java
+++ baekjoon/lv2/Main_1436.java
@@ -1,1 +1,52 @@
+package com.baekjoon.bruteforce;
 
+import java.util.Scanner;
+
+/**
+ * 2022.6.3(금) 23h55
+ * @author greenpianorabbit
+ * 종말의 숫자 = 적어도 3개의 연속된 '6'이 들어가는 수 -> n(입력; 10,000보다 작거나 같은 자연수)번째로 작은 종말의 숫자 출력
+ * e.g. 1 입력 -> 666 출력, 2 입력 -> 1666 출력, .., 10 입력 -> 9666 출력, 11 입력 -> 10666 출력..
+ */
+public class Main_1436 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+//		int numToCompare = 666;
+		int count = 0;
+		
+		/*
+		while (true) {
+			String stringToCompare = String.valueOf(numToCompare);
+			
+			if (stringToCompare.contains(n)) {
+				count += 1;
+				System.out.println(count);
+			} else {
+				numToCompare += 1;
+			}
+			
+		}
+		*/
+		
+		// The literal 999999999999 of type int is out of range 
+		for (int i = 666; i < 1000000000; i++) {
+			if (String.valueOf(i).contains("666")) {
+				count += 1;
+				if (count == n) {
+					System.out.println(i);
+				} else {
+					count--;
+					continue;
+				}
+			} else {
+				continue;
+			}
+		}
+		
+//		sc.close();
+	}
+
+}
