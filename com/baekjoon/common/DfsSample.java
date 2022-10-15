d40d2abcaf8082991bf9838fcf5cc9220e984364
Eunyoung KANG <greenkey20@gmail.com>
2022-10-15 23:57:39 +0900

    feat: add problem-solving guide

---
+++ com/baekjoon/common/DfsSample.java
@@ -0,0 +1,54 @@
+package com.baekjoon.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 2022.10.15(토) 12h
+// 참고자료 출처: https://nahwasa.com/m/172
+public class DfsSample {
+    // 인스턴스 변수
+    private int max = 0;
+    private int n, k;
+    private int[] arr;
+
+    // 멤버 메서드
+    public void dfs(int cnt, int num) {
+        if (cnt == 0)  return;
+
+        num *= 10;
+        if (num >= n) return;
+
+        for (int i = 0; i < k; i++) {
+            int tmp = num + arr[i];
+            if (tmp > n) continue;
+            if (tmp > max) max = tmp;
+            dfs(cnt +1, tmp);
+        }
+    }
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()); // br.readLine() 관련 예외 처리 필요
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new int[k];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < k; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        dfs(0, 0);
+
+        System.out.println(max);
+    }
+
+    /* psvm을 제출해야 하는데,
+    static main()에는 인스턴스 멤버들을 사용할 수 없고 static/클래스 멤버들만 사용할 수 있으니까,
+    main() 내용은 최소화하고, public solution() 함수로 한 번 래핑해서 만들어두면 좋음 -> 프로그래머스 등 다른 플랫폼에도 비슷하게 코드 재활용 가능
+     */
+    public static void main(String[] args) throws IOException {
+        new DfsSample().solution();
+    }
+}
---
+++ com/baekjoon/common/platformGuide.md
@@ -0,0 +1,20 @@
+# 백준 Java 풀이 시 tips/guide
+
+## 1. public class 이름은 Main이어야 함
+- Main 이외의 클래스를 써야 하는 경우, public이 아닌 클래스나 inner 클래스로 만들어서 사용
+
+## 2. import 부분도 함께 제출해야 함
+
+## 3. public static void main() 함수를 포함하는 프로그램 클래스를 제출하는 것임
+
+## 4. 입력 받는 것부터 출력하는 부분까지 작성해야 함
+- 템플릿 만들어서 '프로그래머스'에서와 비슷하게 문제를 풀 수 있도록 하기
+- 
+### ✅ 효율적인 입력
+Scanner는 느림 → BufferReader 사용하기
+
+### ✅ 효율적인 출력
+
+### References
+- [자바로 백준 풀 때의 팁 및 주의점 (boj java)](https://nahwasa.com/m/172)
+- [<취준> 코딩테스트 준비하기 (with. 백준 & 프로그래머스)](https://haesoo9410.tistory.com/351)
