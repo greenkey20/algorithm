37a61b2481148096ff8bc623a6419a43e69fc531
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-11-21 00:24:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 344 ms, Memory: 22492 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 데이터 입력받기
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 선택 정렬 알고리즘 구현
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    // arr[i]와 arr[j]의 자리 바꾸기
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
