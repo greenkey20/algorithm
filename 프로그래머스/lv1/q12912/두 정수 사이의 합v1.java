3e00826b506820ce55e4cd418060fe1766ec5a20
Eunyoung KANG <greenkey20@gmail.com>
2022-11-27 00:51:49 +0900

    fix: update problem-solving exercise [math]

--- 프로그래머스/lv1/q12912/두 정수 사이의 합v1.java
+++ 프로그래머스/lv1/q12912/두 정수 사이의 합v1.java
@@ -1,6 +1,6 @@
 package 프로그래머스.lv1.q12912;
 
-class Solution {
+class SolutionV1 {
     // 2022.11.26(토) 23h50
     public long solution(int a, int b) {
         long answer = 0;
--- 프로그래머스/lv1/q12912/두 정수 사이의 합v2.java
+++ 프로그래머스/lv1/q12912/두 정수 사이의 합v2.java
@@ -1,4 +1,6 @@
-class Solution {
+package 프로그래머스.lv1.q12912;
+
+class SolutionV2 {
     // 2022.11.26(토) 23h50
     public long solution(int a, int b) {
         long answer = 0;        
