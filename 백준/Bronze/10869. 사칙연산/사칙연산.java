d5622d7226c335248414ae10050bd0b663332c7e
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-11-28 11:14:40 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2022.11.28(월) 11h
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // v2 입력 방식 수정
+        String[] strs = br.readLine().split(" ");
+        int A = Integer.parseInt(strs[0]);
+        int B = Integer.parseInt(strs[1]);
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+        br.close();
+    }
+}
