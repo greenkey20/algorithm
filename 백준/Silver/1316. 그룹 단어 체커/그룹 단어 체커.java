f312a091a02ce8a52f683daa0c9d6988f357ceb5
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2022-12-23 21:41:19 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+// 2022.12.23(금) 0h35 -> v1 1h30 틀렸습니다
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] words = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
+        }
+
+        // 문제 풀이 로직
+
+        // 출력
+        System.out.println(numOfGroupWords(words));
+
+    }
+
+    public static int numOfGroupWords(String[] words) {
+        int result = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == 1) {
+                result++;
+                continue;
+            }
+
+            Set<Character> alphabets = new HashSet<>();
+
+            for (int j = 0; j < words[i].length() - 1; j++) {
+                char thisCh = words[i].charAt(j);
+                char nextCh = words[i].charAt(j + 1);
+
+                alphabets.add(thisCh);
+
+                if (thisCh != nextCh) {
+                    if (!alphabets.contains(nextCh)) {
+                        alphabets.add(nextCh);
+                    } else {
+                        break;
+                    }
+                }
+
+//                System.out.println(alphabets);
+                if (j == words[i].length() - 2) result++;
+            }
+        }
+
+        return result;
+    }
+}
