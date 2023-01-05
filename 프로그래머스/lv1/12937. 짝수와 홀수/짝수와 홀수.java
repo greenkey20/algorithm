189f010b2d30b6e240b9335629e8530019547fa4
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-01-06 07:36:26 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,11 @@
+class Solution {
+    // 2023.1.6(금) 7h35
+    public String solution(int num) {
+        String answer = "";
+        
+        if (num % 2 == 0) answer = "Even";
+        else answer = "Odd";
+            
+        return answer;
+    }
+}
