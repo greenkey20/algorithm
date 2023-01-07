d30387c692ee4587228fec24fef76e6026e6471b
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-01-07 17:23:27 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    // 2023.1.7(토) 17h15
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        int copyX = x;
+        while (copyX > 0) {
+            sum = sum + copyX % 10;
+            copyX = copyX / 10;
+        }
+        
+        return x % sum == 0;
+    }
+}
