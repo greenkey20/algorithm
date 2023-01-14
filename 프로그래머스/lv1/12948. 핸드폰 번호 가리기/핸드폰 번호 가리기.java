beb929ae159d454798645fc06da5024a673e5a48
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-01-15 00:00:32 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,18 @@
+class Solution {
+    // 2023.1.14(토) 23h55
+    public String solution(String phone_number) {
+        //String answer = "";
+        //String last4Digits = phone_number.substring(phone_number.length() - 4, phone_number.length());
+        StringBuilder sb = new StringBuilder();
+        
+    for (int i = 0; i < phone_number.length(); i++) {
+        if (i < phone_number.length() - 4) {
+            sb.append("*");
+        } else {
+            sb.append(phone_number.charAt(i));
+        }
+    }       
+        
+        return sb.toString();
+    }
+}
