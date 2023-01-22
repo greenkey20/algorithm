694ab6624dc07b877e5037b16482407681874114
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-01-22 14:53:25 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int quotient = n; // 3으로 나눈 몫
+//        Stack<Integer> remainders = new Stack<>(); // list에 바로 담아서 앞뒤 반전 3진법을 바로 만들 수도 있음
+        List<Integer> remainders = new ArrayList<>();
+
+        while (quotient > 0) {
+            quotient = n / 3;
+            remainders.add(n % 3);
+            n = quotient;
+        }
+
+        int decimalDigit = 0;
+        for (int i = remainders.size() - 1; i >= 0; i--) {
+            decimalDigit += (int) Math.pow(3.0, i) * remainders.get(remainders.size() - i - 1);
+        }
+
+        return decimalDigit;
+    }
+}
