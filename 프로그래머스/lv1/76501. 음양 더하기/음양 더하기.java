7d0497a08ee0d3035212344e70b4c65bf2228e7b
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-01-23 22:08:54 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    // 2023.1.23(월) 22h
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for (int i = 0; i < absolutes.length; i++) {
+            int absolute = absolutes[i];
+            
+            if (signs[i]) {
+                answer += absolute;
+            } else {
+                answer += absolute * -1;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
