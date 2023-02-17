40571e4f051e9f8820e4f19da1206f7689d4474f
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-02-17 13:23:55 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        int currentNumOfEmptyBottles = 0;
+
+        boolean flag = true;
+        while (flag) {
+            currentNumOfEmptyBottles = n / a * b + n - (n / a) * a;
+            answer = answer + n / a * b;
+            n = currentNumOfEmptyBottles;
+
+            if (currentNumOfEmptyBottles < a) flag = false;
+        }
+        
+        return answer;
+    }
+}
