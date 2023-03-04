1e76a97c7fc02a7f91bc0de282df3ada9166c928
Eunyoung KANG <greenkey20@gmail.com>
2023-03-05 02:44:47 +0900

    feat: add sql exercise

---
+++ src/main/java/com/programmers/lv3/carRentalAvailability.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT car_id, case when '2022-10-16' between start_date and end_date then '대여중'
+                    else '대여 가능'
+    end availability
+from car_rental_company_rental_history
+group by car_id
+order by car_id desc;
