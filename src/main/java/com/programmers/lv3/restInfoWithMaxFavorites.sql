7497a65108a0c99be8e0e3d57465553a882a948f
Eunyoung KANG <greenkey20@gmail.com>
2023-03-21 02:11:03 +0900

    rename: rename folders of sql exercises

--- src/main/java/com/programmers/lv3/restInfoWithMaxFavorites.sql
+++ src/main/java/com/programmers/lv3/restInfoWithMaxFavorites.sql
@@ -11,3 +11,9 @@ from rest_info r1
 group by food_type
 having favorites = (select max(favorites) from rest_info r2 group by food_type having r1.food_type = r2.food_type)
 order by food_type desc;
+
+-- 코드를 입력하세요 2023.3.21(화) 2h5 v3 정답!
+select s.food_type, r.rest_id, r.rest_name, s.favorites
+from (select food_type, max(favorites) favorites from rest_info group by food_type) s, rest_info r
+where s.food_type = r.food_type and s.favorites = r.favorites
+order by food_type desc;
