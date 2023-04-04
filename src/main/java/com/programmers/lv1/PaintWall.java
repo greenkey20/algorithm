8b530c0881e434f8b748b6f2b7364ee4a6f09776
Eunyoung KANG <greenkey20@gmail.com>
2023-04-04 20:35:11 +0900

    fix: update problem-solving exercise [math]

--- src/main/java/com/programmers/lv1/PaintWall.java
+++ src/main/java/com/programmers/lv1/PaintWall.java
@@ -1,10 +1,17 @@
 package com.programmers.lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 // 2023.4.4(화) 19h35 ~ 19h45 v1 54.0 반례가 어떤 경우로 있을까?
-// v2 연습장에 반례 생각하며 알고리즘 생각해봄
+// ~ 20h v2 기존 코드를 어떻게 고치면 반례도 cover할 수 있을까 생각하다가, 갑자기 떠오른 생각으로 작성해봄(원래는 v3 idea를 구현하려고 했었음)
 public class PaintWall {
     public static int solution(int n, int m, int[] section) {
         // 페인트를 다시 칠해야 하는 section 배열의 원소들은 오름차순으로 정렬되어 있음 + 같은 원소가 2번 이상 나타나지 않음
+        /*
         int diff = section[section.length - 1] - section[0] + 1;
         // v2 위 diff 구간 중 안 칠해도 되는 구간의 번호들 구하기 -> 실행해본 뒤 입/출력 예시1 보니 diff 구간에서 누락된 숫자의 위치도 중요한 바, 아래 로직으로는 풀 수 없음
         StringBuilder sb = new StringBuilder();
@@ -30,6 +37,31 @@ public class PaintWall {
         } else {
             return answer + 1;
         }
+         */
+
+        // v3 연습장에 반례 생각하며 알고리즘 생각해봄
+        int answer = 0;
+
+        List<Integer> sectionList = new ArrayList<>();
+        for (int i : section) {
+            sectionList.add(i);
+        }
+
+        for (int i = section[0] + 1; i <= section[section.length - 1]; ) {
+            answer++;
+
+            if (!sectionList.contains(i + m)) {
+                if (i + m > n) {
+                    return answer - 1;
+                } else {
+                    i++;
+                }
+            } else {
+                i += m;
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
