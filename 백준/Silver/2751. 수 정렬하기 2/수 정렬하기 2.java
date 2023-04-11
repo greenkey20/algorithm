cce232d0c942e68e2aba0a75d76be97d74efe1fe
Eunyoung <87472526+greenkey20@users.noreply.github.com>
2023-04-11 21:48:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1712 ms, Memory: 126156 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+// 2023.4.11(화) 21h30
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 데이터 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 문제 해결 = 정렬
+        Arrays.sort(arr);
+
+        // 출력 = 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i : arr) {
+            bw.write(String.valueOf(i));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
