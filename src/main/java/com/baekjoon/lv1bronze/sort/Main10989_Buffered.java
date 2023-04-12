265fc90e5cf7a20cf3bbc423bd41462cbab81d46
Eunyoung KANG <greenkey20@gmail.com>
2023-04-13 00:32:25 +0900

    fix: update problem-solving exercise [sort]

--- src/main/java/com/baekjoon/lv1bronze/sort/Main10989_Buffered.java
+++ src/main/java/com/baekjoon/lv1bronze/sort/Main10989_Buffered.java
@@ -2,24 +2,32 @@ package com.baekjoon.lv1bronze.sort;
 
 import java.io.*;
 
-// 2023.4.12(수) 23h35 입/출력 방식 바꿔봄
+// 2023.4.12(수) 23h35 ~ 23h59 입/출력 방식 바꿔봄 = 합병 정렬로는 통과 안 됨
 // 합병 정렬 알고리즘은 2022.5.31(화) ~ 2022.6.1(수)에 작성한 것
 public class Main10989_Buffered {
     public static void main(String[] args) throws IOException {
         // 데이터 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 정렬 대상 수의 개수 = 1 ~ 10,000,000
+        int n = Integer.parseInt(br.readLine()); // 정렬 대상 수의 개수 = 1 ~ 10,000,000 vs 2751번 문제에서는 1,000,000 이하
+        // reference https://st-lab.tistory.com/107에 따르면 계수 정렬로 풀거나 Arrays.sort()(평균 O(nlogn), 최악의 경우 O(n^2)의 시간복잡도)로 풀 수 있다고 함
 
+        // 배열 대상 숫자는 10,000보다 작거나 같은 자연수 + 중복 입력 가능
         int[] inputArr = new int[n];
         for (int i = 0; i < n; i++) {
             inputArr[i] = Integer.parseInt(br.readLine());
         }
 
         // 문제 해결 = 오름차순 정렬
-        int[] sortedArr = mergeSort(inputArr, n);
+        // v1 합병 정렬 = 통과 안 됨
+//        int[] sortedArr = mergeSort(inputArr, n);
+
+        // 2023.4.13(목) 0h10 ~ 0h25 v2 계수 정렬 = 맞았습니다
+        int[] sortedArr = countingSort(inputArr);
 
         // 출력 = 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력
-        // 방법1) StringBuilder 사용 = 23h59 메모리 초과
+        // v1 방법1) 합병 정렬 + StringBuilder 사용 = 23h59 메모리 초과
+        // v2 방법1) 계수 정렬 + StringBuilder 사용 = 1804ms 통과
+        /*
         StringBuilder sb = new StringBuilder();
 
         for (int i : sortedArr) {
@@ -27,9 +35,10 @@ public class Main10989_Buffered {
         }
 
         System.out.println(sb);
+         */
 
-        // 방법2) BufferedWriter 사용 = 23h59 시간 초과
-        /*
+        // v1 방법2) 합병 정렬 + BufferedWriter 사용 = 23h59 시간 초과
+        // v2 방법2) 계수 정렬 + BufferedWriter 사용 = 2144ms 통과
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i : sortedArr) {
             bw.write(String.valueOf(i));
@@ -39,9 +48,9 @@ public class Main10989_Buffered {
         br.close();
         bw.flush();
         bw.close();
-         */
     }
 
+    // 합병 정렬
     public static int[] mergeSort(int[] arr, int n) {
         if (n > 1) {
             int mid = n / 2;
@@ -103,5 +112,33 @@ public class Main10989_Buffered {
         }
 
         return mergedArr;
+    }
+
+    // 계수 정렬
+    public static int[] countingSort(int[] inputArr) {
+        int[] counts = new int[10001];
+        int[] sortedArr = new int[inputArr.length];
+
+        // step1) counts 배열 만들기
+        for (int i = 0; i < inputArr.length; i++) { // 성능 O(n)
+            int num = inputArr[i];
+            counts[num]++;
+        }
+
+        // step2) counts 배열 내용물을 누적합으로 바꿈
+        for (int i = 1; i < counts.length; i++) { // 성능 O(k)
+            counts[i] += counts[i - 1];
+        }
+
+        // step3) sortedArr 만들기
+        for (int i = 0; i < inputArr.length; i++) { // 성능 O(n)
+            int num = inputArr[i];
+            int count = counts[num];
+
+            sortedArr[count - 1] = num;
+            counts[num]--;
+        }
+
+        return sortedArr;
     }
 }
