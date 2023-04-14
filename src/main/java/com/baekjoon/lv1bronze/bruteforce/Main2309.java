8942ba56be7a9200341fb39b79711a00864bd2b2
Eunyoung KANG <greenkey20@gmail.com>
2023-04-14 23:24:00 +0900

    fix: update problem-solving exercise [brute force]

--- src/main/java/com/baekjoon/lv1bronze/bruteforce/Main2309.java
+++ src/main/java/com/baekjoon/lv1bronze/bruteforce/Main2309.java
@@ -7,24 +7,44 @@ import java.util.Arrays;
 
 // 2023.4.14(금) 21h50 ~ 22h45 v1 references 보고 썼는데, 코드 이해도 정확히 안 되고, 생각한대로 동작하지 않음
 /* 9개 중 7개를 순서없이 뽑는 '조합'
+
 references
-https://data-make.tistory.com/467
+1. 조합 일반
 https://velog.io/@cgw0519/알고리즘-순열-중복순열-조합-중복조합-총정리
 https://minhamina.tistory.com/38
+2. 문제 풀이
+https://data-make.tistory.com/467
+https://anott.tistory.com/25
  */
 public class Main2309 {
+    // v2
+    static int n = 9, r = 7, k = 100;
+    static int[] dwarfs = new int[n];
+    static int[] realDwarfs = new int[r];
+    static int[] results = new int[r];
+    static boolean ckEnd = false;
+
     public static void main(String[] args) throws IOException {
         // 데이터 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] inputs = new int[9];
-        for (int i = 0; i < 9; i++) {
-            inputs[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            dwarfs[i] = Integer.parseInt(br.readLine());
         }
 
         // 문제 해결 로직
-        Arrays.sort(inputs);
-        int r = 7;
+        Arrays.sort(dwarfs);
+
+        // v2 22h50 ~ 23h20 references 보고 일단 써봄
+        combination(0, 0, 0);
+
+        // 출력
+        for (int i : results) {
+            System.out.println(i);
+        }
+
+        // v1
+        /*
         boolean[] visited = new boolean[inputs.length];
 
         StringBuilder sb = new StringBuilder();
@@ -37,11 +57,35 @@ public class Main2309 {
             }
             System.out.println(sb);
         }
-
+         */
     }
 
-    public static void combination(int[] arr, boolean[] visited, int start, int depth, int r) {
+    // v2
+    public static void combination(int count, int start, int sum) {
+        // 합이 100인 7명 난쟁이를 찾았으니까, 더 이상 진행할 필요 없음
+        if (ckEnd) return;
 
+        if (count == r) { // 7명을 뽑은 조합이 되었고
+            if (sum == k) { // 7명의 키의 합이 100이면
+                // 이 조합을 '답'으로 적어둠
+                // Manual array copy = 내가 작성한 것
+                /*
+                for (int i = 0; i < realMembers.length; i++) {
+                    results[i] = realMembers[i];
+                }
+                 */
+                // IntelliJ의 suggestion
+                System.arraycopy(realDwarfs, 0, results, 0, realDwarfs.length);
+
+                ckEnd = true;
+            }
+            return;
+        }
+
+        for (int i = start; i < dwarfs.length; i++) {
+            realDwarfs[count] = dwarfs[i];
+            combination(count + 1, i + 1, sum + dwarfs[i]);
+        }
     }
 
     public static boolean combinationDraft(int[] arr, boolean[] visited, int start, int depth, int r) {
