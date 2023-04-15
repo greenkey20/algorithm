7c77a135bb3db57a17a80feba693d37e82fe127a
Eunyoung KANG <greenkey20@gmail.com>
2023-04-15 22:42:45 +0900

    fix: update problem-solving exercise [array]

--- src/main/java/com/programmers/lv1/Budget.java
+++ src/main/java/com/programmers/lv1/Budget.java
@@ -3,8 +3,29 @@ package com.programmers.lv1;
 import java.util.Arrays;
 
 public class Budget {
-    // 2023.1.9(월) 7h30 -> 8h25 sout으로 확인 시 입/출력 예시 결과대로 결과 찍히긴 하는데, 코드 이해 못하고 따라 친 것임 + 제출 시 대부분의 테스트케이스 시간 초과
+    // 2023.4.15(토) 22h25 1월에 풀 때는 조합을 생각했었는데, 다른 방법이 생각나서 작성해 보기로 함 -> 22h40 맞았습니다
+    // 처음에 너무 어렵게 생각했었나..?
     public static int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            answer++;
+
+            if (sum > budget) {
+                answer--;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    // 2023.1.9(월) 7h30 -> 8h25 sout으로 확인 시 입/출력 예시 결과대로 결과 찍히긴 하는데, 코드 이해 못하고 따라 친 것임 + 제출 시 대부분의 테스트케이스 시간 초과
+    public static int solutionDraft(int[] d, int budget) {
         int answer = 0;
 
         /* idea1 = d의 원소들을 오름차순으로 정렬(작은 수부터 더해야 보다 더 많은 원소들을 합산 가능)
@@ -19,28 +40,30 @@ public class Budget {
         int i = 1;
 
         for (; i < d.length; i++) {
-            if (combination1(d, visited, 0, i, budget)) {
-                return i;
-            }
+//            if (combination1(d, visited, 0, i, budget)) {
+//                return i;
+//            }
         }
 
         return i;
     }
 
-    public static boolean combination1(int[] d, boolean[] visited, int start, int i, int budget) {
-        boolean result = false;
+    public static void combination1(int[] d, boolean[] visited, int start, int depth, int budget) {
+//        boolean result = false;
 
-        if (i == 0) {
-            return budget == sum(d, visited);
+//        if (i == 0) {
+//            return budget == sum(d, visited);
+//        }
+
+        for (int i = start; i < d.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                combination1(d, visited, i + 1, depth + 1, budget);
+                visited[i] = false;
+            }
         }
 
-        for (int j = start; j < d.length; j++) {
-            visited[i] = true;
-            result = combination1(d, visited, start + 1, i - 1, budget);
-            visited[i] = false;
-        }
-
-        return result;
+//        return result;
     }
 
     public static int sum(int[] d, boolean[] visited) {
