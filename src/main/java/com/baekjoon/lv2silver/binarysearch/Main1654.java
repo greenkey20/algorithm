d5e72c59ffdf2866c792a32bd94d4d191b531b34
Eunyoung KANG <greenkey20@gmail.com>
2023-04-25 00:20:18 +0900

    fix: update problem-solving exercise [binary search]

--- src/main/java/com/baekjoon/lv2silver/binarysearch/Main1654.java
+++ src/main/java/com/baekjoon/lv2silver/binarysearch/Main1654.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 // 2023.4.17(월) 18h40 ! 18h55 v1 틀렸습니다 = 최대의 answer가 안 나옴
 // Main2805와 비슷한 문제로 추천받음
-/* 2023.4.24(월) 22h55 reference https://st-lab.tistory.com/269 공부
+/* 2023.4.24(월) 23h55 reference https://st-lab.tistory.com/269 공부
  */
 public class Main1654 {
     public static void main(String[] args) throws IOException {
@@ -27,17 +27,18 @@ public class Main1654 {
             lines[i] = Integer.parseInt(br.readLine()); // 각 랜선의 길이는 2^31-1보다 작거나 같은 자연수
         }
 
-        System.out.println("가지고 있는 랜선의 수 k = " + k + ", n개의 랜선으로 만들고자 함 = " + n + ", lines 배열 = " + Arrays.toString(lines)); // todo
-
         // 문제 해결 로직
+        // 2023.4.25(화) 0h5 upperbound 방식 binary search로 수정해봄 -> 0h15 제출 시 '틀렸습니다'
         Arrays.sort(lines);
+        System.out.println("가지고 있는 랜선의 수 k = " + k + ", n개의 랜선으로 만들고자 함 = " + n + ", lines 배열 = " + Arrays.toString(lines)); // todo
 
         int left = 0;
         int right = lines[k - 1];
         int answer = 0;
+        int mid = 0;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        while (left < right) {
+            mid = (left + right) / 2;
             int numOfLines = 0;
 
             for (int i = 0; i < k; i++) {
@@ -46,15 +47,18 @@ public class Main1654 {
                 numOfLines = numOfLines + line / mid;
             }
 
-            if (numOfLines == n) {
+            /*if (numOfLines == n) {
                 answer = mid;
                 break;
-            } else if (numOfLines < n) {
+            } else*/
+            if (numOfLines < n) {
                 right = mid / 2;
             } else {
                 left = mid + 1;
             }
         }
+
+        answer = mid + 1;
 
         // 결과 출력
         System.out.println(answer);
