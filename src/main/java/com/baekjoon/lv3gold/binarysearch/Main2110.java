de9b0bcadb67814511fc16b8ddb1b8ae33b862ee
Eunyoung KANG <greenkey20@gmail.com>
2023-05-01 23:07:41 +0900

    fix: update problem-solving exercise [binary search]

--- src/main/java/com/baekjoon/lv3gold/binarysearch/Main2110.java
+++ src/main/java/com/baekjoon/lv3gold/binarysearch/Main2110.java
@@ -9,8 +9,9 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 // 2023.4.26(수) 23h45 ~ 0h30 v1 while문 안 thisRouter 관련 array index out of bounds exception -> 2023.4.29(토) 23h50 v2 이 예외는 처리하였으나, 프로그램 로직 안 맞는 듯
-// -> 2023.4.30(일) 1h5 아직 동작 안 하는데, 어떻게 수정해야 할지 모르겠다 -> 일단 프로그램을 작은 단위 메서드/모듈들로 나눠봄 according to Eddie's tips
+// -> 2023.4.30(일) 1h5 아직 동작 안 하는데, 어떻게 수정해야 할지 모르겠다 -> 일단 프로그램을 작은 단위 메서드/모듈들로 나눠봄 according to Eddie's tips + 2023.5.1(월) 22h10 메서드 호출 구조 조금 수정? 안 하는 게 나을 듯
 /* 문제 이해에 시간 걸렸는데, reference https://st-lab.tistory.com/277에 따라 "포인트는 집 간 최소 거리 이상일 때 설치가 가능하다"
+내 코드가 동작을 안 하기에 2023.5.1(월) 22h15 reference logic 보고 옴 v3 = 입/출력예시에 대해서는 (드디어) 동작하는데, 23h5 제출 시 '틀렸습니다'
  */
 public class Main2110 {
     public static int n;
@@ -20,10 +21,10 @@ public class Main2110 {
     // main function/method
     public static void main(String[] args) throws IOException {
         // 형식1) 백준 웹사이트 요구 사항 = through data layer v2
-//        presentSolution(baekjoonIO());
+        presentSolution(baekjoonIO());
 
         // 형식2) 테스트용 = through data layer v1
-        presentSolution(myTestCase1());
+//        presentSolution(myTestCase1());
     }
 
     // 데이터 입력받기 v1 = data layer v1
@@ -64,7 +65,9 @@ public class Main2110 {
         List<Integer> housesList = new ArrayList<>(Arrays.asList(houses));
         System.out.println("houses 배열 = " + Arrays.toString(houses)); // todo
 
+        // v1, v2 나의 구현
         // houses 간 거리에 대해 이분탐색
+        /*
         int left = houses[0];
         int right = houses[n - 1] - houses[0]; // upperBound의 의미를 오늘은 조금 다르게 정의해서 풀어보기로 함
         int mid = 0;
@@ -102,6 +105,37 @@ public class Main2110 {
 
             if (numOfRouters < c) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+         */
+
+        // 2023.5.1(월) 22h15 reference 참고 후 구현
+        int left = houses[0];
+        int right = houses[n - 1] - houses[0];
+//        int right = (houses[n - 1] - houses[0]) / (n - 1) + 1;
+        int mid = 0;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            System.out.println("left = " + left + ", right = " + right + ", mid = " + mid); // todo
+
+            int numOfRouters = 1;
+            int numOfLastRouter = houses[0];
+
+            for (int i = 1; i < n; i++) {
+                int numOfThisHouse = houses[i];
+
+                if (numOfThisHouse - numOfLastRouter >= mid) {
+                    numOfLastRouter = numOfThisHouse;
+                    numOfRouters++;
+                    System.out.println("가장 최근 공유기 설치된 집 = " + numOfLastRouter + ", 설치된 공유기 총 개수 = " + numOfRouters); // todo
+                }
+            }
+
+            if (numOfRouters < c) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
