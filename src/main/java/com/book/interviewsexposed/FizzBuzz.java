02e9d4d862544bc9cc2b9f712f0c844b7464cdc6
Eunyoung KANG <greenkey20@gmail.com>
2023-09-23 01:42:00 +0900

    fix: update solution

--- src/main/java/com/book/interviewsexposed/FizzBuzz.java
+++ src/main/java/com/book/interviewsexposed/FizzBuzz.java
@@ -21,4 +21,27 @@ public class FizzBuzz {
 
         return sb.toString();
     }
+
+    // 1h30 책 참고
+    public static String alternativeFizzBuzz(int n) {
+        if (n < 1) return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            String word = toWord(3, i, "Fizz") + toWord(5, i, "Buzz");
+
+            if (word.equals("")) {
+                sb.append(i).append(" ");
+            } else {
+                sb.append(word).append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String toWord(int divisor, int num, String word) {
+        return num % divisor == 0 ? word : "";
+    }
 }
--- src/test/java/com/book/interviewsexposed/FizzBuzzTest.java
+++ src/test/java/com/book/interviewsexposed/FizzBuzzTest.java
@@ -4,7 +4,7 @@ package com.book.interviewsexposed;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.book.interviewsexposed.FizzBuzz.printFizzBuzz;
+import static com.book.interviewsexposed.FizzBuzz.alternativeFizzBuzz;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -20,33 +20,38 @@ public class FizzBuzzTest {
         int n = 0;
 
         // when
-        String actual = printFizzBuzz(n);
+//        String actual = printFizzBuzz(n);
+        String actual = alternativeFizzBuzz(n);
 
         // then
         assertNull(actual);
     }
 
     @Test
-    @DisplayName("n = 1로 주어진 경우 테스트") // edge case
+    @DisplayName("n = 1로 주어진 경우 테스트")
+        // edge case
     void testCase2() {
         // given
         int n = 1;
 
         // when
-        String actual = printFizzBuzz(n);
+//        String actual = printFizzBuzz(n);
+        String actual = alternativeFizzBuzz(n);
 
         // then
         assertThat(actual, is(equalTo("1 ")));
     }
 
     @Test
-    @DisplayName("n = 7로 주어진 경우 테스트") // 첫번째 edge case 3과 5 + 두번째 3의 배수인 6을 테스트
+    @DisplayName("n = 7로 주어진 경우 테스트")
+        // 첫번째 edge case 3과 5 + 두번째 3의 배수인 6을 테스트
     void testCase3() {
         // given
         int n = 7;
 
         // when
-        String actual = printFizzBuzz(n);
+//        String actual = printFizzBuzz(n);
+        String actual = alternativeFizzBuzz(n);
 
         // then
         assertThat(actual, is(equalTo("1 2 Fizz 4 Buzz Fizz 7 ")));
@@ -60,7 +65,8 @@ public class FizzBuzzTest {
         int n = 16;
 
         // when
-        String actual = printFizzBuzz(n);
+//        String actual = printFizzBuzz(n);
+        String actual = alternativeFizzBuzz(n);
 
         // then
         assertThat(actual, is(equalTo("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 ")));
