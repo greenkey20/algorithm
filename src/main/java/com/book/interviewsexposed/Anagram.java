985edf6fed8d9fb08dfd3f127b2dc11b19d3e017
Eunyoung KANG <greenkey20@gmail.com>
2023-09-26 14:10:44 +0900

    feat: add a sample problem-solving interview question4 [book2]

---
+++ src/main/java/com/book/interviewsexposed/Anagram.java
@@ -0,0 +1,21 @@
+package com.book.interviewsexposed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2023.9.26(화) 14h5
+public class Anagram {
+
+    /**
+     * Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 애너그램이라고 한다.
+     * 주어진 단어의 목록에서 특정 단어에 대한 전체 애너그램 목록을 반환하는 알고리즘을 작성하라.
+     * @param words
+     * @param word
+     * @return
+     */
+    public static List<String> getAnagrams(List<String> words, String word) {
+        List<String> anagrams = new ArrayList<>();
+
+        return anagrams;
+    }
+}
